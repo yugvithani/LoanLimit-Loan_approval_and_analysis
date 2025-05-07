96e49edf1cc7f2670a0b57cc277624c087d325e4
Yug Vithani <vithaniyug01@gmail.com>
2025-05-07 15:15:34 +0530

    add manager endpoints and email format

--- backend/ServerApi/src/main/java/com/example/ServerApi/repositories/ManagerRepository.java
+++ backend/ServerApi/src/main/java/com/example/ServerApi/repositories/ManagerRepository.java
@@ -1,7 +1,10 @@
 package com.example.ServerApi.repositories;
 
 import com.example.ServerApi.entity.Manager;
+import jakarta.transaction.Transactional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
